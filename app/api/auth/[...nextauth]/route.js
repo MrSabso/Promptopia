ca1716abc9a2a9349473ff0d1ef9e2b7330011aa
Saba <godmode7616@gmail.com>
2025-02-09 17:04:56 +0400

    Implement user authentication with NextAuth, add user model, and update navigation for session handling

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,10 @@
 import NextAuth from "@node_modules/next-auth";
 import GoogleProvider from 'next-auth/providers/google'
 
+
+import { connectToDB } from "@utils/database";
+import User from "@models/user";
+
 const handler = NextAuth({
     providers:[
         GoogleProvider({
@@ -9,13 +13,35 @@ const handler = NextAuth({
         })
     ],
     async session({ session }) {
+        const sessionUser = await User.findOne({
+            email: session.user.email
+        })
 
+        session.user.id = sessionUser._id.toString();
+
+        return session;
     },
     async signIn({ profile }) {
         try {
-            
+            await connectToDB();
+
+            // check if user already exists
+            const userExists = await User.findOne({
+                email: profile.email
+            });
+            // if not, create new and save
+            if(!userExists){
+                await User.create({
+                    email: profile.email,
+                    username: profile.name.replace("/\s/g,").toLowerCase(),
+                    image: profile.picture
+                }) 
+            }
+
+            return true;
         } catch (error) {
-            
+            console.log(error)
+            return false;
         }
     },
 })
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,20 +5,19 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 import { useEffect, useState } from 'react';
 
 const Nav = () => {
-
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
 
       setProviders(response);
     }
 
-    setProviders();
+    setUpProviders();
   }, [])
 
   return (
@@ -35,9 +34,10 @@ const Nav = () => {
         Promptopia
       </p>
       </Link>
+      
       { /* Desktop Navigation */}
       <div className='sm:flex hidden'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex gap-3 md:gap-5'>
             <Link href="/create-prompt"
             className="black_btn">
@@ -73,7 +73,7 @@ const Nav = () => {
 
       { /* Mobile Navigation */}
       <div className='sm:hidden flex relative'>
-        {isUserLoggedIn ?(
+        {session?.user ?(
           <div className='flex'>
             <Image src="/assets/images/logo.svg"
               width={37}
